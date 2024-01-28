ee4d982fa4e67a61e8cea2a16c538bc36b12dc5b
Ashfaque Alam <67138411+ashfaque@users.noreply.github.com>
2024-01-28 13:14:26 +0530

    updated bug in setup.py file

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
           'django-redis'
           # 'djangorestframework'
           # 'Django'
-          'AshLogger>=0.4'
+          , 'AshLogger>=0.4'
           ,
       ],
   classifiers=[
